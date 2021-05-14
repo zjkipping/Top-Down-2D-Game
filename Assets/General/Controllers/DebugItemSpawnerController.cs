7e10277f4b995fce7054d1df6c0b3e3af39fce21
Zachary Kipping <zjkipping@hotmail.com>
2021-05-14 17:52:05 -0500

    Learned coroutines, somewhat of a start to having dropped items bounce

--- Assets/General/Controllers/DebugItemSpawnerController.cs
+++ Assets/General/Controllers/DebugItemSpawnerController.cs
@@ -13,8 +13,7 @@ public class DebugItemSpawnerController : MonoBehaviour
         int itemPoolIndex =  Random.Range(0, itemPool.Count);
         ItemObject item = itemPool[itemPoolIndex];
         int amount = Random.Range(1, item.MaxAmount);
-        Vector3 direction = new Vector2(Random.Range(-1f, 1f) * 1.5f, Random.Range(-1f, 1f) * 1.5f);
-        Vector3 position = player.transform.position + direction;
+        Vector3 position = player.transform.position;
         GameObject go = Instantiate(item.DroppedItemPrefab, position, Quaternion.identity);
         go.GetComponent<DroppedItemController>().Initialize(item, amount);
     }
--- Assets/Items/Controllers/DroppedItemController.cs
+++ Assets/Items/Controllers/DroppedItemController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 using UnityEngine;
 
 public class DroppedItemController : MonoBehaviour
@@ -13,26 +15,81 @@ public class DroppedItemController : MonoBehaviour
 
     private PlayerController targetPlayer = null;
 
-    private float delayTargetingPlayerTime = 2;
-    private float currentDelay = 0;
+    private float delayPickupTime = 2f;
+    private bool delayPickup;
 
-    public bool CanCollect => !delayPickup;
-    private bool delayPickup = false;
+    private bool canCollect = false;
+    public bool CanCollect => canCollect;
 
-    public void Initialize(ItemObject _item, int _amount, bool _delayPickup = false) {
+    public void Initialize(ItemObject _item, int _amount, bool _delayPickup = true) {
+        GetComponent<SpriteRenderer>().sprite = _item.Sprite;
         item = _item;
         amount = _amount;
         delayPickup = _delayPickup;
-        GetComponent<SpriteRenderer>().sprite = item.Sprite;
+        StartCoroutine(ThrowAndBounceItem(new Vector2(1, 0)));
     }
 
-    private void Update() {
-        if (delayPickup) {
-            currentDelay += Time.deltaTime;
-            if (currentDelay >= delayTargetingPlayerTime) {
-                delayPickup = false;
+    private IEnumerator ThrowAndBounceItem(Vector2 direction) {
+        Vector2 start = transform.position;
+        float distance = 1.5f;
+        float startRadius = 0.5f;
+        int bounces = 3;
+        int currentBounce = 1;
+
+        while(currentBounce < bounces) {
+            Vector2 end = start + (direction * (distance / currentBounce));
+            Vector2 difference = end - start;
+            float span = difference.magnitude;
+            float radius = startRadius / currentBounce;
+
+            // Override the radius if it's too small to bridge the points.
+            float absRadius = Mathf.Abs(radius);
+            if(span > 2f * absRadius)
+                radius = absRadius = span/2f;
+
+            Vector2 perpendicular = new Vector2(difference.y, -difference.x)/span;
+            perpendicular *= Mathf.Sign(radius) * Mathf.Sqrt(radius*radius - span*span/4f);
+
+            Vector2 center = start + difference/2f + perpendicular;
+
+            Vector2 toStart = start - center;
+            float startAngle = Mathf.Atan2(toStart.y, toStart.x);
+
+            Vector2 toEnd = end - center;
+            float endAngle = Mathf.Atan2(toEnd.y, toEnd.x);
+
+            // Choose the smaller of two angles separating the start & end
+            float travel = (endAngle - startAngle + 5f * Mathf.PI) % (2f * Mathf.PI) - Mathf.PI;
+
+            float progress = 0f;
+            while (progress < 1f) {
+                float angle = startAngle + progress * travel;
+                transform.position = center + new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * absRadius;
+                progress += Time.deltaTime/0.2f;
+                yield return null;
             }
+
+            transform.position = end;
+            start = end;
+            currentBounce++;
+            yield return null;
         }
+
+        if (delayPickup) {
+            StartCoroutine(DelayPickup());
+        } else {
+            canCollect = true;
+        }
+    }
+
+    private IEnumerator DelayPickup() {
+        canCollect = false;
+        float currentDelay = 0;
+        while(currentDelay <= delayPickupTime) {
+            currentDelay += Time.deltaTime;
+            yield return null;
+        }
+        canCollect = true;
     }
 
     private void FixedUpdate() {
@@ -42,7 +99,7 @@ public class DroppedItemController : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (CanCollect && other.tag == EntityTags.Player) {
+        if (canCollect && other.tag == EntityTags.Player) {
             InventoryController inventoryController = other.gameObject.GetComponent<InventoryController>();
             if (inventoryController.Inventory.CanItemFit(item, amount)) {
                 SetTarget(other.gameObject.GetComponent<PlayerController>());
@@ -51,7 +108,7 @@ public class DroppedItemController : MonoBehaviour
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        if (CanCollect && other.tag == EntityTags.Player) {
+        if (canCollect && other.tag == EntityTags.Player) {
             RemoveTarget(other.gameObject.GetComponent<PlayerController>());
         }
     }
